9e5d297e1113e425b7a562bfa3b628d197710f98
salehAli100 <salahjas@yahoo.com>
2018-02-01 23:24:22 +0100

    sort method added
    sort method added:   to sort the combinations in descending order in regard to jccard values.

--- Testing-Techniques/src/testingTechniques/Test.java
+++ Testing-Techniques/src/testingTechniques/Test.java
@@ -8,30 +8,27 @@ public class Test {
 	String name = null;
 	List<String> Case = null;
 	Test test;
-	List<Test> tests = new ArrayList<>();
+	List<Test> tests = new ArrayList<>(); // holds test cases
 
-	HashMap<String, Double> map = new HashMap<String, Double>();
-	TestCasesComparator Comparator = new TestCasesComparator(map);
-	Map<String, Double> sortedMap = new TreeMap<String, Double>(Comparator);
+	Map<String, Double> map = new HashMap<String, Double>(); // (pair, jccard)
+	Map<String, Double> Map_Sorted;
 
-	List<List> AllCombinations = new ArrayList<>();
-	List Combination;
-
-	Test() {
-	}
+	Test() {}  
 
 	Test(String name, List<String> TestingCase) {
 		this.name = name;
 		this.Case = TestingCase;
 	}
-// this method is to read the file and return a list of Test objects, every object has test case name(string) and the functions or items(list) of test case
+
 	List<Test> readInput(File file) throws FileNotFoundException {
+		// System.out.println(" ------------readInput()--------------");
 		Scanner s = new Scanner(file);
 		while (s.hasNext()) {
 			// 1) read line -> T1 = A, B, C, D 2) split by "=". As result we get two items
 			// which assigned to str_sorted [].
 			String str_raw = s.nextLine();
 			String[] str_sorted = str_raw.split("=");
+			
 			// str_sorted[0]--> ex: "T1". str_sorted[1]--> holds test cases set ex:
 			// "A,B,C,D"
 			String testCase_name = str_sorted[0];
@@ -39,18 +36,20 @@ public class Test {
 
 			test = new Test(testCase_name, test_Functions);
 			tests.add(test);
+			// System.out.println("test case : " + test);
+
 		}
+		System.out.println(" ------------------------------");
 
 		s.close();
 		return tests;
 	}
 
-	// this method manipulate the tests' arraylist to get the jaccard of two Case(s)
-	// and sort the tests in ascending order regarding the jccard index
-	List pair() {
+	// this method operate on the tests' arraylist to get the jaccard of two Case(s)
+	// and sort the tests in ascending order
+	Map pair() {
 
 		for (int i = 0; i < tests.size(); i++) {
-
 			List<String> original_List = tests.get(i).Case;
 
 			for (int j = i + 1; j < tests.size(); j++) {
@@ -59,47 +58,43 @@ public class Test {
 				Double union = 0.0;
 
 				List<String> looping = tests.get(j).Case;
-				// List<String> unique_combination= original_arrList;
 
-				System.out.println(original_List + "  original_arrList --> " + tests.get(i).name);
-				System.out.println(looping + "  loop");
 				// intersection
 				for (String x : looping) {
 					if (original_List.contains(x))
 						intersection++;
 				}
-				
 				// union
 				union = (original_List.size() + looping.size()) - intersection;
 				// jccard
-				Double precntage = intersection / union;
+				Double jccard = intersection / union;
 				String pair = " " + tests.get(i).name + " " + tests.get(j).name;
 
-				map.put(pair, precntage);
+				map.put(pair, jccard);
 
-				Combination = new ArrayList();
-				Combination.add(pair);
-				Combination.add(precntage);
-
-				AllCombinations.add(Combination);
-
+				System.out.println(original_List + "  original_arrList --> " + tests.get(i).name);
+				System.out.println(looping + "  loop");
 				System.out.println(intersection + " intersection");
 				System.out.println(union + " union");
-				System.out.println(precntage + " % " + "  jccard");
+				System.out.println(jccard + " % " + "  jccard");
 
-				System.out.println(" ------------------------------------------");
-
+				System.out.println(" ------------------------------");
 			}
 		}
-		System.out.println(" --------------  Pair()  ------------------");
 
-	//	System.out.println("one item      " + AllCombinations.get(0));
-		System.out.println("allCombinations: " + AllCombinations);
-		test.sortedMap.putAll(test.map);
-		return AllCombinations;
+		return map;
 	}
 
-	
+	Map sort(Map<String, Double> map) {
+
+		TestCasesComparator Comparator = new TestCasesComparator(map);
+		Map_Sorted = new TreeMap<String, Double>(Comparator);
+		Map_Sorted.putAll(map);
+
+		return Map_Sorted;
+
+	}
+
 	public String toString() {
 		return name + "=" + Case;
 	}
