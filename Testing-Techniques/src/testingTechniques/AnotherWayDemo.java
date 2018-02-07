e294c645ab5f57cc84222040f6539f258c64f496
salehAli100 <salahjas@yahoo.com>
2018-02-07 19:36:07 +0100

    Update AnotherWayDemo.java

--- Testing-Techniques/src/testingTechniques/AnotherWayDemo.java
+++ Testing-Techniques/src/testingTechniques/AnotherWayDemo.java
@@ -1,1 +1,21 @@
 
+package testingTechniques;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class AnotherWayDemo {
+	
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		
+		File file = new File("TestCases_2.txt");
+
+		AnotherWay another = new AnotherWay();
+		another.read.readInput(file);
+
+		another.select();
+	}
+
+}
