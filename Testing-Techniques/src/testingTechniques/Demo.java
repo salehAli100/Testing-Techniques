ad2dc1541c0a6f1cb6c261d4f88e33d18c8baea3
salehAli100 <salahjas@yahoo.com>
2018-02-07 19:27:37 +0100

    Refactoring

--- Testing-Techniques/src/testingTechniques/Demo.java
+++ Testing-Techniques/src/testingTechniques/Demo.java
@@ -1,21 +1,44 @@
-package testingTechniques;
+package Test_techniques;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class Demo {
+public class JccardDemo {
 	
 	
 	public static void main(String[] args) throws FileNotFoundException {
+		
 
-		Test test = new Test();
-		File file = new File("testCases.txt");
-		test.readInput(file);
-		test.pair();
-	
+		File file = new File("TestCases_1.txt");
+		Jac_index jac = new Jac_index();
+		jac.operation.readInput(file);
+		jac.pair();
+		
 
-		test.sortedMap.putAll(test.map);
-		System.out.println("Ascending order : " + test.sortedMap);
+		System.out.println("Test Cases :       " +  jac.operation.getTestCases() );  // 
+		//System.out.println("Pass Key T1 --> get this value ----> " +  jac.operation.getTestCases().get("T1") +"\n" );  // 
+
+		System.out.println("Test cases' names :  " + jac.operation.getTestCases_names() );  // 
+		//System.out.println("one test case's name :  " + jac.operation.getTestCases_names().get(0) +"\n" );  // 
+
+		System.out.println("A list of all sets of functions : " + jac.operation.getFunctions_Lists()  );  // Or -->   operation.getAll_testcases().values()
+		//System.out.println("pick one of set of  functions :   " + jac.operation.getFunctions_Lists().get(0)  );  
+		//System.out.println("pick one of the  functions :       " + jac.operation.getFunctions_Lists().get(0).get(0) +"\n");  
+		
+		System.out.println("Unique Functions List : "+jac.operation.getUnique_functions());  // or :-->>>  operation.All_unique_functions
+		//System.out.println("a function of unique Functions List : "+jac.operation.getUnique_functions().get(0)+ "\n");  // or :-->>>  operation.All_unique_functions
+		
+		
+		System.out.println("\n"+ "--------------Final output---------------------" +"\n");
+
+
+		System.out.println("Jccard of all combinations :   " + jac.similarity);
+		System.out.println("ordered by jccard values : "+ jac.sort(jac.similarity));
+
+
+
+		
+		
 
 	}
 
