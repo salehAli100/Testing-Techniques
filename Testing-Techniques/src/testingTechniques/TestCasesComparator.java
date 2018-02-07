209f4425c38206a018acdedb9cb2fc65989e9566
salehAli100 <salahjas@yahoo.com>
2018-02-07 19:40:25 +0100

    Update TestCasesComparator.java

--- Testing-Techniques/src/testingTechniques/TestCasesComparator.java
+++ Testing-Techniques/src/testingTechniques/TestCasesComparator.java
@@ -1,24 +1,23 @@
 package testingTechniques;
 
-
 import java.util.Comparator;
 import java.util.Map;
 
-
+// this class is used to sort the map in jac_idex by values....sort().
 class TestCasesComparator implements Comparator<String> {
-    Map<String, Double> map;
+	Map<String, Double> map;
 
     public TestCasesComparator(Map<String, Double> map) {
         this.map = map;
     }
 
-	
     @Override
 	 public int compare(String a, String b) {
-	        if (map.get(a) >= map.get(b)) {
+	        if (map.get(a) >= map.get(b)) {    //map.get(k) --> value
 	            return -1;
 	        } else {
 	            return 1;
 	        } 
-	    }  
+	    } 
+
 }
